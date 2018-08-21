d105c90471faa29d5554dc9b3e9d312ef5b2c69c
Jonathan Rainer <jr776@york.ac.uk>
2018-08-21 14:36:02 +0100

    Initial Commit

---
+++ chuushutsu/chuushutsu.py
@@ -0,0 +1,16 @@
+class Chuushutsu(object):
+
+    def __init__(self):
+
+    def run(self):
+        # Take in some raw C code, and construct the appropriate testbench
+        # Compile it and run the simulation using that benchmark
+        # Take the data and create the new queriable graph structure
+        # Postprocess the graph to construct the requested, required, contention list elements
+        # Scan over the graph to find potential re-orderings
+        # Create a schedule of memory
+        return 0
+
+if __name__ == "__main__":
+    system = Chuushutsu()
+    system.run()
