10bc99d8f24dc9eaaf41803f725a4acd46a81a31
Jonathan Rainer <jr776@york.ac.uk>
2018-08-22 14:24:23 +0100

    Successfully Integrated Shiji

--- chuushutsu/chuushutsu.py
+++ chuushutsu/chuushutsu.py
@@ -1,9 +1,17 @@
+from pathlib import Path
+
+from shiji.shiji import Shiji
+
+
 class Chuushutsu(object):
 
     def __init__(self):
+        self.shiji = Shiji(Path("input", "templates"), Path("logs"), Path("output", "memory_contents"), Path("temp"),
+                           "/opt/riscv/bin/", 256, 65536)
 
-    def run(self):
+    def run(self, benchmark_path):
         # Take in some raw C code, and construct the appropriate testbench
+        test = self.shiji.run(Path("input", "benchmarks", "fdct.c"))
         # Compile it and run the simulation using that benchmark
         # Take the data and create the new queriable graph structure
         # Postprocess the graph to construct the requested, required, contention list elements
@@ -11,6 +19,7 @@ class Chuushutsu(object):
         # Create a schedule of memory
         return 0
 
+
 if __name__ == "__main__":
     system = Chuushutsu()
-    system.run()
+    system.run(Path("input", "benchmarks", "test.c"))
